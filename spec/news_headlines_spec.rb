fe186542c1479344b245c5e8d43207625ed3529a
Sergio Correa <correa.sergio@gmail.com>
2017-11-03 01:23:50 +0000

    Setup spec folder with spec file and spec helper

---
+++ spec/news_headlines_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe NewsHeadlines do
+  it "has a version number" do
+    expect(NewsHeadlines::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
