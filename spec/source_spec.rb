011c9b76553da45f2c9597da49f71e03f131e7f0
Sergio Correa <correa.sergio@gmail.com>
2017-11-05 21:39:48 +0000

    Add #new_from_json test to source_spec

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe NewsHeadlines::Source do
     end
   end
 
+  let(:news_hash) {{"id"=>"abc-news-au",
+    "name"=>"ABC News (AU)",
+    "description"=> "Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.",
+    "url"=>"http://www.abc.net.au/news",
+    "category"=>"general",
+    "language"=>"en",
+    "country"=>"au",
+    "urlsToLogos"=>
+     {"small"=>"",
+      "medium"=>"",
+      "large"=>""},
+    "sortBysAvailable"=>["top"]}}
+
+  describe "#new_from_json" do
+    it "creates Source object with hash containing news source info and sets Source attributes from hash key,value pairs" do
+      abc = NewsHeadlines::Source.new_from_json(news_hash)
+      expect(abc.name).to eq("ABC News (AU)")
+      expect(abc.category).to eq("general")
+    end
+  end
+
 end
