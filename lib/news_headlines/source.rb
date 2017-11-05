a338ea0c7caf936e95d3f20ff4b553c29590db7a
Sergio Correa <correa.sergio@gmail.com>
2017-11-05 20:30:33 +0000

    Create initialize method for Source class

--- lib/news_headlines/source.rb
+++ lib/news_headlines/source.rb
@@ -1,5 +1,13 @@
 class NewsHeadlines::Source
-attr_accessor :name, :category, :articles
+  attr_accessor :name, :description, :country, :category, :articles
+
+  def initialize(name = nil, description = nil, country = nil, category = nil)
+    @name = name
+    @description = description
+    @country = country
+    @category = category
+  end
 
 end
+
 puts "Ruby is reading Source.rb"
