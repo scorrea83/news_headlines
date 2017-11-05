bad70c33548186a1d35b88d1217457c4dae1ab4d
Sergio Correa <correa.sergio@gmail.com>
2017-11-05 21:28:28 +0000

    Add #initialize test.

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -2,5 +2,12 @@ require "spec_helper"
 
 RSpec.describe NewsHeadlines::Source do
 
+  describe "#initialize" do
+    it "creates Source object and sets attributes" do
+      newsweek = NewsHeadlines::Source.new(name = "Newsweek", description = nil, country = "US", category = "general")
+      expect(newsweek.name).to eq("Newsweek")
+      expect(newsweek.category).to eq("general")
+    end
+  end
 
 end
