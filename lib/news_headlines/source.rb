99507b5de82f75e8dda105185330525bbbc691b2
Sergio Correa <correa.sergio@gmail.com>
2017-11-05 21:58:57 +0000

    Add self.all method to Source class.

--- lib/news_headlines/source.rb
+++ lib/news_headlines/source.rb
@@ -22,23 +22,27 @@ class NewsHeadlines::Source
   end
   #refactor:attempt to use mass assignment(use source hash info)
 
+  def self.all
+    @@all
+  end
+
 end
 
 puts "Ruby is reading Source.rb"
 #abc_news = NewsHeadlines::Source.new_from_json(abc_hash)
 #newsweek = NewsHeadlines::Source.new(name = "Newsweek", description = nil, country = "US", category = "general")
-{"id"=>"abc-news-au",
-  "name"=>"ABC News (AU)",
-  "description"=> "Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.",
-  "url"=>"http://www.abc.net.au/news",
-  "category"=>"general",
-  "language"=>"en",
-  "country"=>"au",
-  "urlsToLogos"=>
-   {"small"=>"",
-    "medium"=>"",
-    "large"=>""},
-  "sortBysAvailable"=>["top"]}
+# {"id"=>"abc-news-au",
+#   "name"=>"ABC News (AU)",
+#   "description"=> "Australia's most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.",
+#   "url"=>"http://www.abc.net.au/news",
+#   "category"=>"general",
+#   "language"=>"en",
+#   "country"=>"au",
+#   "urlsToLogos"=>
+#    {"small"=>"",
+#     "medium"=>"",
+#     "large"=>""},
+#   "sortBysAvailable"=>["top"]}
 # hash = {
 #   "name" => "Time"
 #   "description" = nil
