77c9be0c0cbf650990af18de0771a1221f8adf5b
Sergio Correa <correa.sergio@gmail.com>
2017-11-10 04:02:49 +0000

    Add Article.initialize test to article_spec

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -2,4 +2,13 @@ require "spec_helper"
 
 RSpec.describe NewsHeadlines::Article do
 
+  describe "#initialize" do
+    it "creates Article object and sets attributes" do
+      article = NewsHeadlines::Article.new(title = "Breaking News", author = "reporter", news_source = "source object", description = "article summary", url = "http://fake.website.com", published_at = "017-11-09T20:05:47Z")
+      expect(article.title).to eq("Breaking News")
+      expect(article.news_source).to eq("source object")
+    end
+  end
+
+
 end
