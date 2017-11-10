7b40c56e9984731c9b5fc14dbe2e598d151c128c
Sergio Correa <correa.sergio@gmail.com>
2017-11-10 19:38:01 +0000

    Fix Article #initialize and #new_from_json and respective tests.
    Remove all mention of Article @news_source variable from these methods.  Setting this variable for the new Article object will be dealt with in a Article.add_news_source method that will be called in Api.make_articles method where Article.new_from_json was called.

--- lib/news_headlines/article.rb
+++ lib/news_headlines/article.rb
@@ -1,4 +1,24 @@
 class NewsHeadlines::Article
-attr_accessor :title, :author, :source, :url, :description, :published_at
+  attr_accessor :title, :author, :news_source, :description, :url, :published_at
+  @@all = []
+
+  def initialize(title = nil, author = nil, description = nil, url = nil, published_at = nil)
+    @title = title
+    @author = author
+    @description = description
+    @url = url
+    @published_at = published_at
+    @@all << self
+  end
+  #FIX: news_source needs to be Source object, maybe create a #add_source method that can then be called at the same time the article object is being created in the API.make_articles method?
+  def self.new_from_json(article)
+    self.new(
+      article["title"],
+      article["author"],
+      article["description"],
+      article["url"],
+      article["published_at"]
+    )
+  end
 
 end
--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -4,9 +4,28 @@ RSpec.describe NewsHeadlines::Article do
 
   describe "#initialize" do
     it "creates Article object and sets attributes" do
-      article = NewsHeadlines::Article.new(title = "Breaking News", author = "reporter", news_source = "source object", description = "article summary", url = "http://fake.website.com", published_at = "017-11-09T20:05:47Z")
+      article = NewsHeadlines::Article.new(title = "Breaking News", author = "reporter", description = "article summary", url = "http://fake.website.com", published_at = "017-11-09T20:05:47Z")
       expect(article.title).to eq("Breaking News")
-      expect(article.news_source).to eq("source object")
+      expect(article.description).to eq("article summary")
+    end
+  end
+
+  let(:article_hash) {{
+      "author" => "Heather DinichESPN Senior Writer ",
+      "title" => "Survival of the fittest in the sprint to the CFP finish",
+      "description" => "It's survive and advance Week 11, with playoff elimination games with Notre Dame-Miami and TCU-Oklahoma, and the top two, Georgia and Alabama, face ranked opponents on the road.",
+      "url" => "http://www.espn.com/college-football/story/_/page/gamedaykickoff110917/survive-advance-cfp-elimination-games-week-11",
+      "urlToImage" => "http://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2017%2F1108%2Fr286333_1296x729_16%2D9.jpg",
+      "publishedAt" => "2017-11-09T14:00:00Z"
+    }}
+
+  describe "#new_from_json" do
+    it "creates Article object with hash argument containing article info and sets Article attributes from hash key,value pairs" do
+      article = NewsHeadlines::Article.new_from_json(article_hash)
+      expect(article.author).to eq("Heather DinichESPN Senior Writer ")
+      expect(article.title).to eq("Survival of the fittest in the sprint to the CFP finish")
+      expect(article.news_source).to eq(nil)
+
     end
   end
 
