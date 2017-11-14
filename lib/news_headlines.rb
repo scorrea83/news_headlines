6790dce372f9c3e72f0c447c00b702c9ed8e382c
Sergio Correa <correa.sergio@gmail.com>
2017-11-14 02:57:32 +0000

    Clean news_headlines.rb

--- lib/news_headlines.rb
+++ lib/news_headlines.rb
@@ -2,13 +2,12 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-
-require "news_headlines/version"
+require_relative "./news_headlines/version"
 require_relative "./news_headlines/cli"
 require_relative "./news_headlines/api"
 require_relative "./news_headlines/source"
 require_relative "./news_headlines/article"
 
-# module NewsHeadlines
-#   # Your code goes here...
-# end
+module NewsHeadlines
+  # Your code goes here...
+end
